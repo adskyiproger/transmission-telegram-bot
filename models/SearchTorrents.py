d88880d8507c8da21b59f4ecd6b22c76d9833954
adskyiproger <vadym.mudryi@gmail.com>
2020-11-16 13:06:30 +0000

    Added search results caching

--- models/SearchTorrents.py
+++ models/SearchTorrents.py
@@ -3,6 +3,7 @@ from telegram import InlineKeyboardButton
 from models.SearchRUTOR import SearchRUTOR
 from models.SearchEZTV import SearchEZTV
 from models.SearchKAT import SearchKAT
+import hashlib
 
 class SearchTorrents:
      CLASSES={ "nnmclub" : SearchNonameClub,
@@ -10,23 +11,32 @@ class SearchTorrents:
                "EZTV" : SearchEZTV,
                "KAT" : SearchKAT
              }
+     CACHE={}
      def __init__(self, name="rutor", search_string="test"):
-         TRACKER=self.CLASSES[name]()
+         self.KEYBOARD=[]
+         self.PAGES={}
+         self.LINKS={}
+         self.POSTS={}
          x=search_string
          print("Name {0} with {1}".format(name,search_string))
-         TRACKER.search(x)
-        
-         n_posts=len(TRACKER.POSTS)
+         srch_hash=hashlib.md5(str(name+search_string).encode('utf-8')).hexdigest()
+         if srch_hash in self.CACHE.keys():
+             print("Using value from cache")
+             self.POSTS=self.CACHE[srch_hash]
+         else:
+             TRACKER=self.CLASSES[name]()
+             TRACKER.search(x)
+             self.POSTS=TRACKER.POSTS
+             self.CACHE[srch_hash]=TRACKER.POSTS
+
+         n_posts=len(self.POSTS)
          n_pages=0
          if n_posts > 0 and n_posts > 5:
               n_pages=n_posts//5
               if n_posts % 5 > 0:
                   n_pages+=1
          print("Pages: {0}, Posts: {1}".format(n_pages,n_posts))
-         self.KEYBOARD=[]
-         self.PAGES={}
-         self.LINKS={}
-         
+        
          for jj in range(1,n_pages+1):
              self.KEYBOARD.append(InlineKeyboardButton(str(jj),callback_data=str(jj)))
 
@@ -34,7 +44,7 @@ class SearchTorrents:
          kk=1
          ii=1
          jj=1
-         for post in TRACKER.POSTS:
+         for post in self.POSTS:
              _message=_message+"\n<b>{0}</b>: {3}  {4}\n<a href='{1}'>Info</a>     [ ▼ /download_{2} ]\n".format(post['title'],post['info'],ii,post['size'],post['date'])
              self.LINKS[str(ii)]=post['dl']
              ii+=1
